4ffbe66963cb99667a2f4b5aef5ba700ee6b56b9
msms1991 <msemcoff@gmail.com>
2021-12-15 00:19:05 -0300

    version11

---
+++ src/holamundo/Holamundo.java
@@ -0,0 +1,147 @@
+
+package holamundo;
+
+import java.util.Scanner;
+
+public class Holamundo {
+    
+    public static void main(String[] args, String hola) {
+        hola = "hola mundo";
+        System.out.println(hola);
+    }
+    
+}
+
+//uso primitivo de tipo flotate: float y double
+       /*
+int a = 3;
+        int b = -a;
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        
+        boolean c = true;
+        boolean d = !c;
+        System.out.println("d = " + d);
+        System.out.println("d = " + d);
+        
+        //incremento
+        //1.preincremento (simbolo antes de la variable)
+        int e = 3;
+        int f = ++e; //primero se incrementa la variable y luego se usa su valor
+        System.out.println("e = " + e);
+        System.out.println("f = " + f);
+        //2 . postincremento (simbolo despues de la variable)
+        
+        int g = 5;
+        int h = g++;//primero se utiliza el valor y luego se incrementa
+        System.out.println("g = " + g);
+        System.out.println("h = " + h);
+        
+        //decremento
+        //1.predecremento
+        int i = 2;
+        int j = --i;
+        System.out.println("i = " + i);//ya esta decrementada
+        System.out.println("j = " + j);//
+        
+        //2.postdecremento
+        int k = 4;
+        int l = k--;//primero se utiliza el valor de la variable y queda pendiente el decremento
+        System.out.println("k = " + k);//tenia pendiente un decremento
+        System.out.println("l = " + l);//
+        
+        
+                
+        
+       
+
+ 
+        int a = 3, b=2;
+        int c = a + 5 - b;
+        System.out.println(c);
+        
+        a += 1; //a = a + 1
+        System.out.println(a);
+     
+        a += 3;//a = a + 3
+        System.out.println(a);
+        
+        a -= 2;//a = a - 2
+        System.out.println(a);
+             
+        a *= 2; //a = a * 2
+        System.out.println(a);
+        
+        a /= 2; //a = a/2
+        System.out.println(a);
+        
+        a %= 2; //a = a%2
+        System.out.println(a);
+
+
+
+
+   int a=3,b=2;
+        int resultado = a + b;
+        System.out.println("Resultado suma = " + resultado);
+        
+        resultado = a - b;
+        System.out.println("Resutlado resta = " + resultado);
+        
+        resultado = a * b;
+        System.out.println("Resultado multiplicacion = " + resultado);
+        
+        float resultados = a / b;
+        System.out.println("Resultado division = " + resultados);
+        
+        resultado = a % b;
+        System.out.println("Resultado modulo = " + resultados);
+        
+        if (b % 2 == 0)
+            System.out.println("Es numero par");
+        else
+            System.out.println("Es numero impar");
+        float numeroFloat = (float)3.4028236E38D;
+        System.out.println("numeroFloat = " + numeroFloat);
+        System.out.println("Valor minimo tipo float " + Float.MIN_VALUE);
+        System.out.println("Valor maximo tipo float" + Float.MAX_VALUE);
+        
+        double numeroDouble = 1.7976931348623157E308;
+        System.out.println("numeroDouble = " + numeroDouble);
+        System.out.println("Valor minimo tipo double " + Double.MIN_VALUE);
+        System.out.println("Valor maximo tipo double" + Double.MAX_VALUE);        
+        
+                
+        
+        System.out.println("Escribe tu nombre: ");
+        Scanner consola = new Scanner(System.in);
+        String usuario = consola.nextLine();
+        System.out.println("Usuario = " + usuario);
+        System.out.println("Escribe el titulo: ");
+        String titulo = consola.nextLine();
+        System.out.println("Resultado: " + titulo + " " + usuario);
+        
+        byte numeroByte = (byte) 129;
+        System.out.println("Valor byte:" + numeroByte);
+        System.out.println("Valor minimo byte:" + Byte.MIN_VALUE);
+        System.out.println("Valor maximo byte:" + Byte.MAX_VALUE);
+        
+        short numeroShort =(short) 32768;
+        System.out.println("numeroShort = " + numeroShort);
+        System.out.println("Valor minimo short:" + Short.MIN_VALUE);
+        System.out.println("Valor maximo short:" + Short.MAX_VALUE); 
+        
+        int numeroInt =  (int) 2147483648L;
+        System.out.println("numeroInt = " + numeroInt);
+        System.out.println("Valor minimo Int:" + Integer.MIN_VALUE);
+        System.out.println("Valor maximo Int:" + Integer.MAX_VALUE); 
+        
+        long numerolong = 92233720368547757L;
+        System.out.println("numerolong = " + numerolong);
+        System.out.println("Valor minimo long:" + Long.MIN_VALUE);
+        System.out.println("Valor maximo long:" + Long.MAX_VALUE); 
+*/
+        
+        
+        
+        
